7476f402205f47c3c32797d114cacb6228187c21
ElisaRodriguez95 <44856961+elisarodriguezholguin@users.noreply.github.com>
2025-11-23 18:01:22 -0500

    Cambios

---
+++ src/counter/components/MyCounterApp.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+export const MyCounterApp = () => {
+
+    const [counter, setCounter] = useState(5)
+    return <div>MyCounterApp</div>;
+
+
+    return(
+        <div style = {{display: 'flex', flexDirection:'column'}}>
+    </div>
+    )
+}
