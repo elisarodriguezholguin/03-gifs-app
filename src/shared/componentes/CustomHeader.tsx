6f002e7a29352e94fa0244a5c5f3c39d2360565d
ElisaRodriguez95 <44856961+elisarodriguezholguin@users.noreply.github.com>
2025-12-29 19:03:19 -0500

    renderHook

--- src/shared/componentes/CustomHeader.tsx
+++ src/shared/componentes/CustomHeader.tsx
@@ -6,9 +6,8 @@ export const CustomHeader = ({title, description}: Props) => {
   return (
     <div className="content-center">
     <h1>{title}</h1>
-    {description  &&  <p>{description}</p>}
+    {<p>{description} </p>}
 
-    {/* <p> Descubre y comparte el gif perfecto</p> */}
   </div>
-  )
-}
+  );
+};
